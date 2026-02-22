35752b97fe4a27202271162a4329bdb7ee953032
streamline-autmations <streamline.automations.hq@gmail.com>
2026-02-22 06:00:24 +0200

    Fix mobile navbar safe-area height and page offset

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -7,7 +7,7 @@ const Layout: React.FC = () => {
   return (
     <div className="flex flex-col min-h-screen relative">
       <Navbar />
-      <main className="flex-grow pt-20 md:pt-24 relative z-10">
+      <main className="flex-grow pt-[calc(var(--nav-h)+16px)] md:pt-24 relative z-10">
         <Outlet />
       </main>
       <Footer />
